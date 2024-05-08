b55cf77cc7a043fe70e51dbd2d9b1ce48fe671d9
Adriana Bamberger <adriana@bamberger.co.nz>
2024-05-08 12:12:18 +1200

    got the nav bar movign to the right links

--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -1,9 +1,16 @@
+import { Link } from 'react-router-dom'
+import data from '../../data/continents.ts'
 function Nav() {
+  const continentName = Object.keys(data)
   return (
     <div>
       <h2>Nav</h2>
       <ul>
-        <li>Build your continent list here</li>
+        {continentName.map((continent, idx) => (
+          <li key={idx}>
+            <Link to={`/continent/${continent}`}>{continent}</Link>
+          </li>
+        ))}
       </ul>
     </div>
   )
